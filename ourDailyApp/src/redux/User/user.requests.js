3a31e2fb2457f469f493eb06bd29ec615be45684
TakYuChan <frankychantakyu@yahoo.com.hk>
2020-10-21 15:31:35 +1300

    SUCCESSFUL DEPLOY

--- ourDailyApp/src/redux/User/user.requests.js
+++ ourDailyApp/src/redux/User/user.requests.js
@@ -5,13 +5,13 @@ export const updateUserInfo = async(formData, imgName) => {
     let res;
     if(imgName) {
         res =  axios
-        .patch("http://localhost:5000/api/v1/users/updateMe", imgName, {withCredentials: true})
+        .patch(`${process.env.REACT_APP_URL}/users/updateMe`, imgName, {withCredentials: true})
         .then(res => res.data.data.user)
         .catch(err => console.error(err));
     }
     else {
         res =  axios
-        .patch("http://localhost:5000/api/v1/users/updateMe", formData, {withCredentials: true})
+        .patch(`${process.env.REACT_APP_URL}/users/updateMe`, formData, {withCredentials: true})
         .then(res => res.data.data.user)
         .catch(err => console.error(err));
     }
