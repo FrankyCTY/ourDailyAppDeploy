8f94af50397d52ef19130d6cbef1aba8cdfce5e4
TakYuChan <frankychantakyu@yahoo.com.hk>
2020-11-09 16:07:45 +1300

    DEPLOY

--- ourDailyApp/src/redux/Auth/auth.sagas.js
+++ ourDailyApp/src/redux/Auth/auth.sagas.js
@@ -115,8 +115,8 @@ function* signInWithEmail({ logInDetails }) {
     yield put(setIsLoggingInTRUE());
     
     // 1) Log in user
-    // const response = yield call(logInUser, [logInDetails, `/api/v1/users/login`]);
-    const response = yield call(logInUser, [logInDetails, `${process.env.REACT_APP_URL}/users/login`]);
+    const response = yield call(logInUser, [logInDetails, `/api/v1/users/login`]);
+    // const response = yield call(logInUser, [logInDetails, `${process.env.REACT_APP_URL}/users/login`]);
     console.log({response});
     // 2) Populate user data
     yield call(populateUserData, response);
